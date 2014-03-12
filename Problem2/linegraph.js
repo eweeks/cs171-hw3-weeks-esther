169288798ed298bbad12cba9956f382b07410bbe
Esther Weeks <esta.weeks@gmail.com>
2014-03-11 18:57:16 -0600

    prob 2, code clean up

--- Problem2/linegraph.js
+++ Problem2/linegraph.js
@@ -27,83 +27,81 @@
     var fill = d3.scale.category10();
     
 
-    svg = d3.select("#vis").append("svg").attr({
-        width: width + margin.left + margin.right,
-        height: height + margin.top + margin.bottom
-    }).append("g").attr({
-            transform: "translate(" + margin.left + "," + margin.top + ")"
+    svg = d3.select("#vis")
+    	.append("svg")
+    	.attr({
+        	width: width + margin.left + margin.right,
+        	height: height + margin.top + margin.bottom
+    	})
+    	.append("g")
+    	.attr({
+        	transform: "translate(" + margin.left + "," + margin.top + ")"
         });
-
 
     d3.csv("timeline.csv", function(data) {
         
-        //Must be a better way of doing this than just coding it all in... 
-        //Takes Data from csv, puts into array
+		//Must be a better way of doing this than just coding it all in... 
+		//Takes Data from csv, puts into array
         data.forEach(function(d, i){
      
         		//UN
-        		if (d.UnitedNationsDepartmentofEconomicandSocialAffairs !== ""){
-        			dataSet.UN.push({"year": d.Year, "pop": parseInt(d.UnitedNationsDepartmentofEconomicandSocialAffairs),
-        				"inter": "no", "name": "UN", });
-        		};
+        	if (d.UnitedNationsDepartmentofEconomicandSocialAffairs !== ""){
+        		dataSet.UN.push({"year": d.Year, "pop": parseInt(d.UnitedNationsDepartmentofEconomicandSocialAffairs),
+        			"inter": "no", "name": "UN", });
+        	};
         		
-        		if(d.UnitedNationsDepartmentofEconomicandSocialAffairs == "" && dataSet.UN.length !== 0 ){
-        			dataSet.UN.push({"year": d.Year, "pop": "", "inter": "yes", "name": "UN", });
-        		};
+        	if(d.UnitedNationsDepartmentofEconomicandSocialAffairs == "" && dataSet.UN.length !== 0 ){
+        		dataSet.UN.push({"year": d.Year, "pop": "", "inter": "yes", "name": "UN", });
+        	};
         		
-        		//US Census
-        		if(d.UnitedStatesCensusBureau !== "" ){
-        			dataSet.USCensus.push({"year": d.Year, "pop": parseInt(d.UnitedStatesCensusBureau),
-        				"inter": "no", "name": "USCensus", });
-        		};
+        	//US Census
+        	if(d.UnitedStatesCensusBureau !== "" ){
+        		dataSet.USCensus.push({"year": d.Year, "pop": parseInt(d.UnitedStatesCensusBureau),
+        			"inter": "no", "name": "USCensus", });
+        	};
         		
-        		if(d.UnitedStatesCensusBureau == "" && dataSet.USCensus.length !== 0 ){
-        			dataSet.USCensus.push({"year": d.Year, "pop": "", "inter": "yes", "name": "USCensus", });
-        		};
+        	if(d.UnitedStatesCensusBureau == "" && dataSet.USCensus.length !== 0 ){
+        		dataSet.USCensus.push({"year": d.Year, "pop": "", "inter": "yes", "name": "USCensus", });
+        	};
         		
-        		//PopBureau
-        		if(d.PopulationReferenceBureau !== ""){
-        			dataSet.populationBureau.push({"year": d.Year, "pop": parseInt(d.PopulationReferenceBureau),
+        	//PopBureau
+        	if(d.PopulationReferenceBureau !== ""){
+        		dataSet.populationBureau.push({"year": d.Year, "pop": parseInt(d.PopulationReferenceBureau),
         			"inter": "no", "name": "populationBureau" });
-        		};
+        	};
         		
-        		if(d.PopulationReferenceBureau == "" && dataSet.populationBureau.length !== 0 ){
-        			dataSet.populationBureau.push({"year": d.Year, "pop": "", "inter": "yes",
+        	if(d.PopulationReferenceBureau == "" && dataSet.populationBureau.length !== 0 ){
+        		dataSet.populationBureau.push({"year": d.Year, "pop": "", "inter": "yes",
         			"name": "populationBureau", });
-        		};
+        	};
         		
-        		//Hyde
-        		if(d.HYDE !== ""){
-        			dataSet.hyde.push({"year":d.Year, "pop": parseInt(d.HYDE), "inter": "no",
+        	//Hyde
+        	if(d.HYDE !== ""){
+        		dataSet.hyde.push({"year":d.Year, "pop": parseInt(d.HYDE), "inter": "no",
         			"name": "hyde", });
-        		};
+        	};
         		
-        		if(d.HYDE == "" && dataSet.hyde.length !== 0 ){
-        			dataSet.hyde.push({"year": d.Year, "pop": "", "inter": "yes", "name": "hyde" });
-        		};
+        	if(d.HYDE == "" && dataSet.hyde.length !== 0 ){
+        		dataSet.hyde.push({"year": d.Year, "pop": "", "inter": "yes", "name": "hyde" });
+        	};
         		
-        		//Maddison
-        		if(d.Maddison !== ""){
-        			dataSet.maddison.push({"year":d.Year, "pop":parseInt(d.Maddison),
-        				"inter": "no", "name": "maddison",  });
-        		}
-        		if(d.Maddison == "" && dataSet.maddison.length !== 0 ){
-        			dataSet.maddison.push({"year": d.Year, "pop": "", "inter": "yes", 
+        	//Maddison
+        	if(d.Maddison !== ""){
+        		dataSet.maddison.push({"year":d.Year, "pop":parseInt(d.Maddison),
+        			"inter": "no", "name": "maddison",  });
+        	}
+        		
+        	if(d.Maddison == "" && dataSet.maddison.length !== 0 ){
+        		dataSet.maddison.push({"year": d.Year, "pop": "", "inter": "yes", 
         			"name": "maddison", });
-        		};
+        	};
         });
   
-	
-		
-		
         return createVis();
     });
 	
-		
-	
-    createVis = function() {
-    
-		//Runs through datasets, finds last entered value, removes empty end data
+	createVis = function() {
+    	//Runs through datasets, finds last entered value, removes empty end data
 		
 		//UN
 		while(dataSet.UN[dataSet.UN.length-1].pop == ""){
@@ -125,20 +123,18 @@
 			dataSet.maddison.pop();
 		}
 		
-		
 		//populationBureau
 		while(dataSet.populationBureau[dataSet.populationBureau.length-1].pop == ""){
 			dataSet.populationBureau.pop();
 		}
-		
 		
 		//interpolate here
 		
     	//Scale for interpolation of data			
     	function iScale(domain, range, year){
     		var inter = d3.scale.linear()
-    				.domain(domain)
-    				.range(range);
+    			.domain(domain)
+    			.range(range);
     				
     		return inter(year);
     	}
@@ -230,15 +226,13 @@
         var xAxis, xScale, yAxis,  yScale;
 
 		//Need to set upper domain in more appropriate manner?
-          xScale = d3.scale.linear().domain([0,2050]).range([bbVis.x, bbVis.w]); 
+        xScale = d3.scale.linear().domain([0,2050]).range([bbVis.x, bbVis.w]); 
 
-		  // example that translates to the bottom left of our vis space:
-		  var visFrame = svg.append("g").attr({
-		      "transform": "translate(" + bbVis.x + "," + (bbVis.y + bbVis.h) + ")",
-			  
-		  });
+		// example that translates to the bottom left of our vis space:
+		var visFrame = svg.append("g").attr({
+			"transform": "translate(" + bbVis.x + "," + (bbVis.y + bbVis.h) + ")",  
+		});
 		  
-		 visFrame.append("rect");
 		  
 		 //find way to select the pop. max ?
         yScale = d3.scale.linear().domain([0, 9400000000]).range([bbVis.h, 0]);
