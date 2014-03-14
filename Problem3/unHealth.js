519078db645e0818b4c8039f0a0f9bffd2d2614a
Esther Weeks <esta.weeks@gmail.com>
2014-03-13 21:48:15 -0600

    Prob 3, adding scales and axis

--- Problem3/unHealth.js
+++ Problem3/unHealth.js
@@ -7,7 +7,7 @@ var margin = {
     left: 50
 };
 
-var width = 960 - margin.left - margin.right;
+var width = 1000 - margin.left - margin.right;
 
 var height = 800 - margin.bottom - margin.top;
 
@@ -18,6 +18,8 @@ bbOverview = {
     h: 50
 };
 
+var padding= 50;
+
 bbDetail = {
     x: 0,
     y: 100,
@@ -26,6 +28,8 @@ bbDetail = {
 };
 
 dataSet = [];
+
+var max;
 
 svg = d3.select("#visUN").append("svg").attr({
     	width: width + margin.left + margin.right,
@@ -57,15 +61,72 @@ var detail = svg
 
 
 d3.csv("unHealth.csv", function(data) {
-	console.log(data);
+
 	var format =d3.time.format("%B-%Y");
 
 	
 	data.forEach(function(d, i){
+		console.log(d);
 		dataSet.push({"date": format.parse(d.AnalysisDate),
-					"count": d.WomensHealth});
+					"count": convertToInt(d.WomensHealth)});
 	});
-	console.log(dataSet);
+
+	//Get max for counts
+	var max = d3.max(dataSet, function(d){
+		return d.count;
+	});
+	
+	//Get min and max for date
+	function date(d) {
+    	return new Date(d.date);
+	}
+
+	// get max and min dates - this assumes data is sorted
+	var minDate = date(dataSet[0]);
+    var maxDate = date(dataSet[dataSet.length-1]);
+	
+	console.log(minDate);
+	console.log(maxDate);
+
+	//Function for overview view
+	function viewA(){
+		console.log(dataSet);
+		
+		//yScale
+		yScale = d3.scale.linear().domain([0, max]).range([bbOverview.h-30, 0]);
+		
+		//xScale
+		xScale = d3.time.scale().domain([minDate, maxDate]).range([15, bbOverview.w-60]);
+		
+		//yAxis
+		var yAxis = d3.svg.axis()
+                  .scale(yScale)
+                  .orient("left")
+                  .ticks(3);
+		
+		//xAxis
+		var xAxis = d3.svg.axis()
+                  .scale(xScale)
+                  .orient("bottom");
+		
+		
+		//draw yAxis
+		overview.append("g")
+    		.attr("class", "axis")
+   		 	.attr("transform", "translate(" + padding + ",0)")
+    		.call(yAxis);
+    		
+    	//draw xAxis
+    	overview.append("g")
+				.attr("class", "axis")
+				.attr("transform", "translate(36," + (bbOverview.h - 30) + ")")
+				.call(xAxis);
+
+	};
+
+	viewA();
+	
+	
 });
 
 var convertToInt = function(s) {
